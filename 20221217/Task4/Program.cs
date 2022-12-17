d695b5cc00797926cc76c1a6d340aeb6858b47a4
TailssTF <tailss5@mail.ru>
2022-12-17 15:41:08 +0700

    20221217

---
+++ 20221217/Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+Console.WriteLine("Введите три числа");
+int a = int.Parse(Console.ReadLine() ?? "0");
+int b = int.Parse(Console.ReadLine() ?? "0");
+int c = int.Parse(Console.ReadLine() ?? "0");
+
+int max = a;
+
+if (b > max) 
+    max = b; 
+if (c > max)
+    max = c;
+Console.WriteLine($"Большее число: {max}");
---
+++ 20221217/Task8/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+if (n > 1)
+{
+    int count = 1;
+    while (count <= n)
+    {
+        if (count % 2 == 0) Console.Write($"{count} ");
+        count++;
+    }
+}
+else Console.WriteLine("Неверный ввод");
