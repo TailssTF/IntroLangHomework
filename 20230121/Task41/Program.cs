f1d68abc3b230024106a35f8bc057fa542a2c7f1
TailssTF <tailss5@mail.ru>
2023-01-22 15:15:57 +0700

    20230121

---
+++ 20230121/Task41/Program.cs
@@ -0,0 +1,77 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+*/
+
+
+// метод, который считывает с консоли (для размерности массива + границы случ. чисел)
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+
+    return result;
+}
+
+/*
+//метод для инициализации массива рандомными числами
+int[] InitArray(int dimension, int leftBound, int rightBound)
+{
+    int[] result = new int[dimension];
+    Random rnd = new Random();
+
+    for (int i = 0; i < dimension; i++)
+    {
+        result[i] = rnd.Next(leftBound, rightBound);
+    }
+    return result;
+}
+*/
+
+int[] ReadArray(int n)
+{
+    int[] result = new int[n];
+    for (int i = 0; i < n; i++)
+        result[i] = GetNumber("");
+    return result;
+}
+
+//метод для печати массива
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int SumPositive(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] > 0) result += array[i];
+    return result;
+}
+
+int number = GetNumber("Введите размер массива");
+Console.WriteLine("Введите элементы массива");
+int[] arr = ReadArray(number);
+PrintArray(arr);
+Console.WriteLine($"Сумма положительных элементов равна {SumPositive(arr)}");
---
+++ 20230121/Task43/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+// метод, который считывает с консоли (для размерности массива + границы случ. чисел)
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+
+    return result;
+}
+
+void Cross (int k1, int b1, int k2, int b2)
+{
+    double temp1 = (b1 - b2);
+    double temp2 = (k1 - k2)*-1;
+    double x = temp1 / temp2;
+    double y = k1 * x + b1;
+    
+    Console.WriteLine($"({x}; {y})");
+}
+
+int k1 = GetNumber("Введите k1");
+int b1 = GetNumber("Введите b1");
+int k2 = GetNumber("Введите k2");
+int b2 = GetNumber("Введите b2");
+Cross(k1, b1, k2, b2);
+
+
